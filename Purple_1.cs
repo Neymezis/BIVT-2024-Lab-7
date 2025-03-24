19824dba3a89f672817f7595db55604f0dcac14b
Neymezis <m2405004@edu.misis.ru>
2025-03-24 21:52:03 +0300

    Update Purple_1.cs

--- Purple_1.cs
+++ Purple_1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab_7
 {
-    internal class Purple_1
+    public class Purple_1
     {
         public class Participant
         {
